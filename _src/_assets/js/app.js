b93c1247224ece7e5a921f73b23d10ec04aa3d2c
Matthias Kretschmann <m@kretschmann.io>
2015-08-23 06:28:48 +0200

    refactor gulpt tasks for faster dev builds

--- _src/_assets/js/app.js
+++ _src/_assets/js/app.js
@@ -1,3 +1,13 @@
+
+//=require ../../../node_modules/webcomponents.js/CustomElements.js
+//=require ../../../node_modules/svg4everybody/svg4everybody.js
+//=require ../../../node_modules/jquery/dist/jquery.js
+//=require ../../../node_modules/masonry-layout/dist/masonry.pkgd.js
+//=require ../../../node_modules/imagesloaded/imagesloaded.js
+//=require ../../../bower_components/time-elements/time-elements.js
+//=require ../../../bower_components/simple-jekyll-search/dest/jekyll-search.js
+
+
 $(ASAP = function() {
 
     siteNavigation.init();
