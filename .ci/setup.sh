2b63b30098f0cbb52ae81f2ac435c2e1764948e9
Matthias Kretschmann <m@kretschmann.io>
2015-08-30 15:16:36 +0200

    cache generated images folder

--- .ci/setup.sh
+++ .ci/setup.sh
@@ -2,7 +2,17 @@
 
 set -e;
 
+echo "$(tput setaf 136)"
+echo "         Installing dependencies "
+echo "============================================="
+echo "$(tput sgr0)" # reset
+
 npm install gulp bower -g
 npm install
 bower install
 bundle install
+
+echo "$(tput setaf 64)" # green
+echo "---------------------------------------------"
+echo "      ✓ done installing dependencies"
+echo "$(tput sgr0)" # reset
