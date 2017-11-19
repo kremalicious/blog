7df57faa5f8af0931e273d3f6203b352451277cd
Matthias Kretschmann <m@kretschmann.io>
2017-11-19 14:03:24 +0100

    makes exif readout work

--- _src/_assets/js/photos.js
+++ _src/_assets/js/photos.js
@@ -4,19 +4,52 @@ window.onload = getExif;
 
 function getExif() {
     const image = document.querySelector('.hentry__teaser img')
-    const imageFileName = image.getAttribute('src').split('/').slice(-1)[0]
 
-    const img = new Image()   // Create new img element
-    img.src = `/media/${imageFileName}`
-    img.addEventListener('load', function () {
-        EXIF.getData(img, function () {
-            var make = EXIF.getTag(this, 'Make')
-            var model = EXIF.getTag(this, 'Model')
-            var makeAndModel = document.querySelector('.exif__make')
-            makeAndModel.innerHTML = `${make} ${model}`
+    EXIF.getData(image, function () {
+        // get individual data
+        const modelvalue = EXIF.getTag(this, 'Model')
+        const shutterspeedvalue = EXIF.getTag(this, 'ExposureTime')
+        const aperturevalue = EXIF.getTag(this, 'FNumber')
+        const exposurevalue = EXIF.getTag(this, 'ExposureBias')
+        const isovalue = EXIF.getTag(this, 'ISOSpeedRatings')
+        const focallengthvalue = EXIF.getTag(this, 'FocalLength')
 
-            console.log(make)
-            console.log(model)
-        })
-    }, false)
+        // inject data
+        if (modelvalue) {
+            const model = document.querySelector('.exif__model')
+            model.innerHTML = modelvalue
+        }
+
+        if (shutterspeedvalue) {
+            const shutterspeed = document.querySelector('.exif__shutterspeed')
+            shutterspeed.innerHTML = `${shutterspeedvalue.numerator}/${shutterspeedvalue.denominator}s`
+        }
+
+        if (aperturevalue) {
+            const aperture = document.querySelector('.exif__aperture')
+            aperture.innerHTML = `ƒ ${aperturevalue}`
+        }
+
+        if (exposurevalue || exposurevalue === 0) {
+            const exposure = document.querySelector('.exif__exposure')
+
+            if (exposurevalue === 0) {
+                exposure.innerHTML = `+/- ${exposurevalue}`
+            } else if (exposurevalue > 0) {
+                exposure.innerHTML = `+ ${exposurevalue}`
+            } else {
+                exposure.innerHTML = `- ${exposurevalue}`
+            }
+        }
+
+        if (isovalue) {
+            const iso = document.querySelector('.exif__iso')
+            iso.innerHTML = `ISO ${isovalue}`
+        }
+
+        if (focallengthvalue) {
+            const focallength = document.querySelector('.exif__focallength')
+            focallength.innerHTML = `${focallengthvalue}mm`
+        }
+    })
 }
