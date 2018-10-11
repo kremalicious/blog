51178c7352b9507c4a8bd220e4e660f54ed7d665
Matthias Kretschmann <m@kretschmann.io>
2018-10-11 21:14:09 +0200

    move QR functionality into own component

--- src/components/molecules/ModalThanks.jsx
+++ src/components/molecules/ModalThanks.jsx
@@ -1,10 +1,9 @@
 import React, { PureComponent } from 'react'
 import { StaticQuery, graphql } from 'gatsby'
-import { QRCode } from 'react-qr-svg'
-import Clipboard from 'react-clipboard.js'
+
 import Web3Donation from '../atoms/Web3Donation'
+import Qr from '../atoms/Qr'
 import Modal from '../atoms/Modal'
-import { ReactComponent as IconClipboard } from '../../images/clipboard.svg'
 import styles from './ModalThanks.module.scss'
 
 const query = graphql`
@@ -39,23 +38,7 @@ class ModalThanks extends PureComponent {
 
                 {Object.keys(author).map((address, i) => (
                   <div key={i} className={styles.coin}>
-                    <h4>{address}</h4>
-                    <QRCode
-                      bgColor="transparent"
-                      fgColor="#6b7f88"
-                      level="Q"
-                      style={{ width: 120 }}
-                      value={author[address]}
-                    />
-                    <pre>
-                      <code>{author[address]}</code>
-                      <Clipboard
-                        data-clipboard-text={author[address]}
-                        button-title="Copy to clipboard"
-                      >
-                        <IconClipboard />
-                      </Clipboard>
-                    </pre>
+                    <Qr title={address} address={author[address]} />
                   </div>
                 ))}
               </div>
