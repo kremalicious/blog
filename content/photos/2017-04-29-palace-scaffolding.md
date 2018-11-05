ab571c1a8262a9b3804a8485f627f48c5624c417
Matthias Kretschmann <m@kretschmann.io>
2018-11-06 00:28:52 +0100

    new photos

---
+++ content/photos/2017-04-29-palace-scaffolding.md
@@ -0,0 +1,9 @@
+---
+type: photo
+date: 2017-04-29T00:00:00.000Z
+
+title: Palace scaffolding
+image: 2017-04-29-palace-scaffolding.jpg
+---
+
+Reconstructed [Berlin Palace](https://en.wikipedia.org/wiki/Berlin_Palace) taking shape.
--- scripts/new.js
+++ scripts/new.js
@@ -22,36 +22,47 @@ spinner.text = `Adding '${title}'.`
 const titleSlug = slugify(title, { lower: true })
 const postsPath = path.join('.', 'content', 'posts')
 const photosPath = path.join('.', 'content', 'photos')
-const date = new Date().toISOString()
-const dateShort = date.slice(0, 10)
 
-const newContents = template
-  .split('TITLE')
-  .join(title)
-  .split('SLUG')
-  .join(titleSlug)
-  .split('DATE')
-  .join(date)
-
-const newContentsPhoto = templatePhoto
-  .split('TITLE')
-  .join(title)
-  .split('SLUG')
-  .join(titleSlug)
-  .split('DATE_LONG')
-  .join(date)
-  .split('DATE_SHORT')
-  .join(dateShort)
-
-const file = `${postsPath}/${dateShort}-${titleSlug}/index.md`
-const filePhoto = `${photosPath}/${dateShort}-${titleSlug}.md`
+let date = new Date().toISOString()
 
 if (isPhoto) {
+  if (process.argv[4]) {
+    date = new Date(process.argv[4]).toISOString()
+  }
+
+  const dateShort = date.slice(0, 10)
+  const filePhoto = `${photosPath}/${dateShort}-${titleSlug}.md`
+
+  const newContentsPhoto = templatePhoto
+    .split('TITLE')
+    .join(title)
+    .split('SLUG')
+    .join(titleSlug)
+    .split('DATE_LONG')
+    .join(date)
+    .split('DATE_SHORT')
+    .join(dateShort)
+
   fs.appendFile(filePhoto, newContentsPhoto, err => {
     if (err) spinner.fail(`Error creating photo post: ${err}`)
     spinner.succeed(`New post '${title}' created.`)
   })
 } else {
+  if (process.argv[3]) {
+    date = new Date(process.argv[3]).toISOString()
+  }
+
+  const dateShort = date.slice(0, 10)
+  const file = `${postsPath}/${dateShort}-${titleSlug}/index.md`
+
+  const newContents = template
+    .split('TITLE')
+    .join(title)
+    .split('SLUG')
+    .join(titleSlug)
+    .split('DATE')
+    .join(date)
+
   fs.outputFile(file, newContents)
     .then(() => fs.readFile(file, 'utf8'))
     .then(() => spinner.succeed(`New post '${title}' created.`))
