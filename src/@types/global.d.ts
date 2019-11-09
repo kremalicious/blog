f59f1bb7c151b66dc9a52b13e4aebe2428392153
Matthias Kretschmann <m@kretschmann.io>
2019-11-09 12:28:19 +0100

    package updates

--- src/@types/global.d.ts
+++ src/@types/global.d.ts
@@ -10,9 +10,9 @@ declare module '*.module.scss' {
 
 declare module '*.svg' {
   import * as React from 'react'
-  export const ReactComponent: React.FunctionComponent<
-    React.SVGProps<SVGSVGElement>
-  >
+  export const ReactComponent: React.FunctionComponent<React.SVGProps<
+    SVGSVGElement
+  >>
   const src: string
   export default src
 }
