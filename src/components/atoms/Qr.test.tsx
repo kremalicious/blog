df9a850f92bc5ae0302d60a38df74bd875758015
Matthias Kretschmann <m@kretschmann.io>
2019-11-23 13:31:04 +0100

    test fixes

--- src/components/atoms/Qr.test.tsx
+++ src/components/atoms/Qr.test.tsx
@@ -1,12 +1,19 @@
-import React from 'react'
-import { render, fireEvent } from '@testing-library/react'
+import React, { Suspense } from 'react'
+import { render, fireEvent, waitForElement } from '@testing-library/react'
 
 import Qr from './Qr'
 
 describe('Qr', () => {
-  it('renders without crashing', () => {
-    const { container } = render(<Qr address="xxx" />)
-    expect(container.firstChild).toBeInTheDocument()
+  test('renders lazy', async () => {
+    const { container } = render(
+      <Suspense fallback="test loading">
+        <Qr address="xxx" />
+      </Suspense>
+    )
+    const lazyElement = await waitForElement(() =>
+      container.querySelector('button')
+    )
+    expect(lazyElement).toBeInTheDocument()
     fireEvent.click(container.querySelector('button'))
   })
 })
--- src/components/atoms/Time.test.tsx
+++ src/components/atoms/Time.test.tsx
@@ -1,9 +1,16 @@
-import React from 'react'
-// import { render } from '@testing-library/react'
-import testRender from '../../../jest/testRender'
+import React, { Suspense } from 'react'
+import { render, waitForElement } from '@testing-library/react'
 
 import Time from './Time'
 
 describe('Time', () => {
-  testRender(<Time date="2017/12/23" />)
+  test('renders lazy', async () => {
+    const { getByText } = render(
+      <Suspense fallback="test loading">
+        <Time date="2017/12/23" />
+      </Suspense>
+    )
+    const lazyElement = await waitForElement(() => getByText(/years ago/i))
+    expect(lazyElement).toBeInTheDocument()
+  })
 })
