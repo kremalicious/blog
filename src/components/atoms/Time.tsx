8cd629a33b3cfbeed7212568242c4fce79167613
Matthias Kretschmann <m@kretschmann.io>
2019-11-23 14:00:07 +0100

    fallbacks

--- src/components/atoms/Time.tsx
+++ src/components/atoms/Time.tsx
@@ -3,18 +3,38 @@ import loadable from '@loadable/component'
 
 const LazyDate = loadable.lib(() => import('date-fns'))
 
+function TimeMarkup({
+  date,
+  format,
+  formatDistance
+}: {
+  date: Date
+  format?: any
+  formatDistance?: any
+}) {
+  const dateIso = date.toISOString()
+
+  return (
+    <time
+      title={format ? format(date, 'yyyy/MM/dd HH:mm') : dateIso}
+      dateTime={dateIso}
+    >
+      {format ? formatDistance(date, Date.now(), { addSuffix: true }) : dateIso}
+    </time>
+  )
+}
+
 export default function Time({ date }: { date: string }) {
   const dateNew = new Date(date)
 
   return (
-    <LazyDate>
+    <LazyDate fallback={<TimeMarkup date={dateNew} />}>
       {({ format, formatDistance }) => (
-        <time
-          title={format(dateNew, 'yyyy/MM/dd HH:mm')}
-          dateTime={dateNew.toISOString()}
-        >
-          {formatDistance(dateNew, Date.now(), { addSuffix: true })}
-        </time>
+        <TimeMarkup
+          date={dateNew}
+          format={format}
+          formatDistance={formatDistance}
+        />
       )}
     </LazyDate>
   )
