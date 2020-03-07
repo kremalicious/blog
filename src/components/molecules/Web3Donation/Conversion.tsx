a9ca7c2444e17ebe8ba01c4c2f9d1b9fa1bdfc3f
Matthias Kretschmann <m@kretschmann.io>
2020-03-07 18:22:12 +0100

    switch to coingecko for prices

--- src/components/molecules/Web3Donation/Conversion.tsx
+++ src/components/molecules/Web3Donation/Conversion.tsx
@@ -3,15 +3,15 @@ import fetch from 'node-fetch'
 import styles from './Conversion.module.scss'
 
 export async function getFiat(amount: number) {
-  const url = 'https://api.coinmarketcap.com/v1/ticker/ethereum/?convert=EUR'
+  const url =
+    'https://api.coingecko.com/api/v3/simple/price?ids=ethereum&vs_currencies=eur%2Cusd'
   const response = await fetch(url)
+
   if (!response.ok) console.error(response.statusText)
   const data = await response.json()
-  /* eslint-disable @typescript-eslint/camelcase */
-  const { price_usd, price_eur } = data[0]
-  const dollar = (amount * price_usd).toFixed(2)
-  const euro = (amount * price_eur).toFixed(2)
-  /* eslint-enable @typescript-eslint/camelcase */
+  const { usd, eur } = data.ethereum
+  const dollar = (amount * usd).toFixed(2)
+  const euro = (amount * eur).toFixed(2)
 
   return { dollar, euro }
 }
