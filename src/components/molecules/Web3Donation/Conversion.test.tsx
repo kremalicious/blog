7421b84fa4fd844acbbbffc5f8e5405a3ad8b7b3
Matthias Kretschmann <m@kretschmann.io>
2020-06-08 12:26:34 +0200

    package updates, fixes for testing-library/react

--- src/components/molecules/Web3Donation/Conversion.test.tsx
+++ src/components/molecules/Web3Donation/Conversion.test.tsx
@@ -1,12 +1,12 @@
 import React from 'react'
-import { render, waitForElement } from '@testing-library/react'
+import { render } from '@testing-library/react'
 
 import Conversion from './Conversion'
 
 describe('Conversion', () => {
   it('renders without crashing', async () => {
-    const { getByText } = render(<Conversion amount={1} />)
-    const lazyElement = await waitForElement(() => getByText(/= €/))
+    const { findByText } = render(<Conversion amount={1} />)
+    const lazyElement = await findByText(/= €/)
     expect(lazyElement).toBeInTheDocument()
   })
 })
--- src/components/molecules/Web3Donation/index.test.tsx
+++ src/components/molecules/Web3Donation/index.test.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { render, waitForElement, fireEvent } from '@testing-library/react'
+import { render, waitFor, fireEvent } from '@testing-library/react'
 import { Web3ReactProvider } from '@web3-react/core'
 import { getLibrary } from '../../../hooks/use-web3'
 
@@ -7,20 +7,16 @@ import Web3Donation from '.'
 
 describe('Web3Donation', () => {
   it('renders without crashing', async () => {
-    const { container, getByText } = render(
+    const { container, findByText } = render(
       <Web3ReactProvider getLibrary={getLibrary}>
         <Web3Donation address="xxx" />
       </Web3ReactProvider>
     )
-    const lazyElement = await waitForElement(() =>
-      container.querySelector('button')
-    )
+    const lazyElement = await waitFor(() => container.querySelector('button'))
     expect(lazyElement).toBeInTheDocument()
 
     fireEvent.click(lazyElement)
-    const message = await waitForElement(() =>
-      getByText(/No Ethereum browser extension detected/)
-    )
+    const message = await findByText(/No Ethereum browser extension detected/)
     expect(message).toBeInTheDocument()
   })
 })
