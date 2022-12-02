21071a1e0fefc0f44c3181593333c0fc78dfbcbc
Matthias Kretschmann <m@kretschmann.io>
2022-12-02 23:44:35 +0000

    production check for env vars

--- src/helpers/umami.ts
+++ src/helpers/umami.ts
@@ -1,7 +1,8 @@
 const UMAMI_SCRIPT_URL = process.env.GATSBY_UMAMI_SCRIPT_URL
 const UMAMI_WEBSITE_ID = process.env.GATSBY_UMAMI_WEBSITE_ID
+const isProduction = process.env.NODE_ENV === 'production'
 
-if (!UMAMI_SCRIPT_URL || !UMAMI_WEBSITE_ID) {
+if (isProduction && (!UMAMI_SCRIPT_URL || !UMAMI_WEBSITE_ID)) {
   throw new Error('Missing Umami environment variables')
 }
 
