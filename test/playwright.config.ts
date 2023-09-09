bcf97e3770e6bd7fb91ad347f47747a9b5138bb8
Matthias Kretschmann <m@kretschmann.io>
2023-09-09 16:03:41 +0100

    ci tweaks

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -26,18 +26,18 @@ export default defineConfig({
       name: 'firefox',
       use: { ...devices['Desktop Firefox'] }
     },
-    {
-      name: 'webkit',
-      use: { ...devices['Desktop Safari'] }
-    },
+    // {
+    //   name: 'webkit',
+    //   use: { ...devices['Desktop Safari'] }
+    // },
     {
       name: 'Mobile Chrome',
       use: { ...devices['Pixel 5'] }
-    },
-    {
-      name: 'Mobile Safari',
-      use: { ...devices['iPhone 12'] }
     }
+    // {
+    //   name: 'Mobile Safari',
+    //   use: { ...devices['iPhone 12'] }
+    // }
 
     // {
     //   name: 'Microsoft Edge',
