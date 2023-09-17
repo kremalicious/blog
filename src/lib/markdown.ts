a7ea295c4188a27bdf44c43080aa4025a789500c
Matthias Kretschmann <m@kretschmann.io>
2023-09-17 12:41:37 +0000

    migrate to createMarkdownProcessor

--- src/lib/markdown.ts
+++ src/lib/markdown.ts
@@ -1,16 +1,7 @@
-// import { unified } from 'unified'
-// import remarkParse from 'remark-parse'
-// import remarkRehype from 'remark-rehype'
-// import rehypeStringify from 'rehype-stringify'
-
-import { renderMarkdown } from '@astrojs/markdown-remark'
+import { createMarkdownProcessor } from '@astrojs/markdown-remark'
 
 export async function markdownToHtml(markdown: string) {
-  const content = await renderMarkdown(markdown, { gfm: true })
-  return content.code
-  //   return await unified()
-  //     .use(remarkParse)
-  //     .use(remarkRehype)
-  //     .use(rehypeStringify)
-  //     .process(markdown)
+  const processor = await createMarkdownProcessor()
+  const { code } = await processor.render(markdown)
+  return code
 }
