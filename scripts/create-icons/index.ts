81d9e983352f1892b3ff697807a19a14da9c14b8
Matthias Kretschmann <m@kretschmann.io>
2023-09-18 01:36:12 +0100

    create-icons test

--- scripts/create-icons/index.ts
+++ scripts/create-icons/index.ts
@@ -23,77 +23,81 @@ const distDir = ps.resolve(currentDir, 'src/images/components')
 // Data related to each icon exported by this package.
 const icons = []
 
-const spinner = ora(
-  `${chalk.bold('[create-icons]')} Create icon components`
-).start()
+export async function generateIcons(distDir: string) {
+  const spinner = ora(
+    `${chalk.bold('[create-icons]')} Create icon components`
+  ).start()
 
-// clean the distribution directory
-await fs.rm(distDir, { force: true, recursive: true })
-await fs.mkdir(distDir, { recursive: true })
+  // clean the distribution directory
+  await fs.rm(distDir, { force: true, recursive: true })
+  await fs.mkdir(distDir, { recursive: true })
 
-// copy the attribute typings file
-await fs.copyFile(
-  ps.resolve(currentDir, 'scripts/create-icons/Props.d.ts'),
-  ps.resolve(distDir, 'Props.d.ts')
-)
+  // copy the attribute typings file
+  await fs.copyFile(
+    ps.resolve(currentDir, 'scripts/create-icons/Props.d.ts'),
+    ps.resolve(distDir, 'Props.d.ts')
+  )
 
-// convert the SVG files into Astro components
-let contentOfIndexJS = '// @ts-nocheck\n'
+  // convert the SVG files into Astro components
+  let contentOfIndexJS = '// @ts-nocheck\n'
 
-for (const src of srcDirs) {
-  for (let filepath of await fs.readdir(src, { encoding: 'utf8' })) {
-    // ignore non-svg files
-    if (!filepath.endsWith('.svg')) continue
+  for (const src of srcDirs) {
+    for (let filepath of await fs.readdir(src, { encoding: 'utf8' })) {
+      // ignore non-svg files
+      if (!filepath.endsWith('.svg')) continue
 
-    // Base name of the SVG.
-    const name = filepath.replace(/\.svg$/, '')
+      // Base name of the SVG.
+      const name = filepath.replace(/\.svg$/, '')
 
-    // get filepath as a full path
-    filepath = ps.resolve(src, filepath)
+      // get filepath as a full path
+      filepath = ps.resolve(src, filepath)
 
-    // Inner contents of the SVG file.
-    const innerSVG = toInnerSvg(await fs.readFile(filepath, 'utf8'))
+      // Inner contents of the SVG file.
+      const innerSVG = toInnerSvg(await fs.readFile(filepath, 'utf8'))
 
-    // Formatted title.
-    const title = name
-      .replace(
-        // uppercase alphabetic characters after the start or a dash
-        /(?<=^|-)([a-z])/g,
-        (_0, $1) => $1.toUpperCase()
+      // Formatted title.
+      const title = name
+        .replace(
+          // uppercase alphabetic characters after the start or a dash
+          /(?<=^|-)([a-z])/g,
+          (_0, $1) => $1.toUpperCase()
+        )
+        .replace(
+          // replace non-alphanumeric characters with space
+          /[^A-Za-z0-9]+/g,
+          ' '
+        )
+        .replace(
+          // respect 'GitHub' brand casing
+          'Github Logo',
+          'GitHub Logo'
+        )
+
+      // Base name, which is the formatted title without spaces (PascalCase)
+      const baseName = title.replace(/ /g, '')
+
+      // write the astro component to a file
+      await fs.writeFile(
+        ps.resolve(distDir, `${baseName}.astro`),
+        toAstroComponent(innerSVG, title),
+        'utf8'
       )
-      .replace(
-        // replace non-alphanumeric characters with space
-        /[^A-Za-z0-9]+/g,
-        ' '
-      )
-      .replace(
-        // respect 'GitHub' brand casing
-        'Github Logo',
-        'GitHub Logo'
-      )
 
-    // Base name, which is the formatted title without spaces (PascalCase)
-    const baseName = title.replace(/ /g, '')
+      // add the astro component export to the main entry `index.ts` file
+      contentOfIndexJS += `\nexport { default as ${baseName} } from './${baseName}.astro'`
 
-    // write the astro component to a file
-    await fs.writeFile(
-      ps.resolve(distDir, `${baseName}.astro`),
-      toAstroComponent(innerSVG, title),
-      'utf8'
-    )
-
-    // add the astro component export to the main entry `index.ts` file
-    contentOfIndexJS += `\nexport { default as ${baseName} } from './${baseName}.astro'`
-
-    icons.push({ name, baseName, title })
+      icons.push({ name, baseName, title })
+    }
   }
+
+  // write the main entry `index.ts` file
+  await fs.writeFile(ps.resolve(distDir, 'index.ts'), contentOfIndexJS, 'utf8')
+
+  spinner.succeed(
+    `${chalk.bold('[create-icons]')} Generated ${
+      icons.length
+    } icons into @images/components.`
+  )
 }
 
-// write the main entry `index.ts` file
-await fs.writeFile(ps.resolve(distDir, 'index.ts'), contentOfIndexJS, 'utf8')
-
-spinner.succeed(
-  `${chalk.bold('[create-icons]')} Generated ${
-    icons.length
-  } icons into @images/components.`
-)
+generateIcons(distDir)
