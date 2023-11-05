e6d5e504b00d2d7a849af933339a29d2ff8ebaa0
Matthias Kretschmann <m@kretschmann.io>
2023-11-05 18:52:40 +0000

    Add display of transaction status link and explorer name in Success component

--- src/features/Web3/components/Success/Success.tsx
+++ src/features/Web3/components/Success/Success.tsx
@@ -1,27 +1,31 @@
 import { $txHash, $isInitSend } from '@features/Web3/stores'
 import { useStore } from '@nanostores/react'
 import styles from './Success.module.css'
+import { useNetwork } from 'wagmi'
 
 export function Success() {
+  const { chain } = useNetwork()
   const txHash = useStore($txHash)
+  console.log(chain)
+
+  const explorerName = chain?.blockExplorers?.default.name
+  const explorerLink = `${chain?.blockExplorers?.default.url}/tx/${txHash}`
 
   return (
     <div className={styles.success}>
-      <h5 className={styles.title}>You're amazing!</h5>
+      <h5 className={styles.title}>You're amazing, thanks!</h5>
 
       <p>
-        Your transaction has been sent. You can check the status on{' '}
-        <a
-          href={`https://etherscan.io/tx/${txHash}`}
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Etherscan
+        Your transaction is on its way. You can check the status on{' '}
+        <a href={explorerLink} target="_blank" rel="noopener noreferrer">
+          {explorerName}
         </a>
         .
       </p>
       <p>
-        <code>0xxxx{txHash}</code>
+        <a href={explorerLink} target="_blank" rel="noopener noreferrer">
+          <code>{txHash}</code>
+        </a>
       </p>
 
       <footer className={styles.actions}>
