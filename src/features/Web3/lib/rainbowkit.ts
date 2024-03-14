86030ada0328f78ac986c96951dbec02d96a66ac
Matthias Kretschmann <m@kretschmann.io>
2024-03-14 11:36:18 +0000

    add arbitrum

--- src/features/Web3/lib/rainbowkit.ts
+++ src/features/Web3/lib/rainbowkit.ts
@@ -1,5 +1,5 @@
 import { type Theme, getDefaultConfig } from '@rainbow-me/rainbowkit'
-import { mainnet, polygon, base, optimism } from 'wagmi/chains'
+import { mainnet, polygon, base, optimism, arbitrum } from 'wagmi/chains'
 
 const PUBLIC_WALLETCONNECT_ID = import.meta.env.PUBLIC_WALLETCONNECT_ID
 const isProduction = import.meta.env.PROD
@@ -11,7 +11,7 @@ if (isProduction && !PUBLIC_WALLETCONNECT_ID) {
 export const wagmiConfig = getDefaultConfig({
   appName: 'kremalicious.com',
   projectId: PUBLIC_WALLETCONNECT_ID,
-  chains: [mainnet, polygon, base, optimism]
+  chains: [mainnet, polygon, base, optimism, arbitrum]
 })
 
 export const theme: Theme = {
