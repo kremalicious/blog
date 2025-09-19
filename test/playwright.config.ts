b0dd4c19284b6c95b522f365327302433297a238
Matthias Kretschmann <m@kretschmann.io>
2025-09-20 00:38:41 +0100

    increase worker count for CI environment

--- test/playwright.config.ts
+++ test/playwright.config.ts
@@ -8,7 +8,7 @@ export default defineConfig({
   fullyParallel: true,
   forbidOnly: !!process.env.CI,
   retries: process.env.CI ? 2 : 0,
-  workers: process.env.CI ? 1 : undefined,
+  workers: process.env.CI ? 2 : undefined,
   /* https://playwright.dev/docs/test-reporters */
   reporter: process.env.CI ? 'github' : 'list',
   /* Shared settings for all the projects below. See https://playwright.dev/docs/api/class-testoptions. */
