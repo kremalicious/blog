b76910345ecccf731b98678652ce06f186468560
Matthias Kretschmann <m@kretschmann.io>
2025-11-15 00:22:44 +0000

    switch to biome for Astro formatting

--- src/features/posts/types/posts.ts
+++ src/features/posts/types/posts.ts
@@ -1,9 +1,14 @@
 import type { CollectionEntry } from 'astro:content'
+import type { ImageMetadataFormatted } from './exif'
 
 export type BlogCollection = 'articles' | 'links' | 'photos'
 
-export type PhotoPost = CollectionEntry<'photos'>
 export type ArticlePost = CollectionEntry<'articles'>
+
+export type PhotoPost = CollectionEntry<'photos'> & {
+  data: { imageMetadata: ImageMetadataFormatted | undefined }
+}
+
 export type LinkPost = CollectionEntry<'links'>
 
 export type BlogPost = CollectionEntry<BlogCollection>
